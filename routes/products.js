6969a8ca7aa7919b18ebc42b7e9198af20e823c2
Pegah <Prashidnia@gmail.com>
2021-09-12 21:18:39 -0400

    creating/adding a new product

--- routes/products.js
+++ routes/products.js
@@ -2,8 +2,13 @@ var express = require("express");
 var router = express.Router();
 
 module.exports = function (db) {
-  router.get("/products", (req, res) => {
-    res.send(db.get("products").value()); //use response object 
+  router.get("/products", (req, res) => { //GET, reading
+    res.send(db.get("products").value()); //use response object to send data back
+  });
+
+  router.post("/products", (req, res) => {
+    const newProduct = req.body;    //get info from request
+    res.send(db.get("products").insert(newProduct).write());  //saving product to file, need to call write() after insert()
   });
   return router;
 };
