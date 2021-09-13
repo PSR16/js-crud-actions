4af22b078dd990b2c0daae705f04e760bf8d9ba4
Pegah <Prashidnia@gmail.com>
2021-09-13 00:10:25 -0400

    delete product

--- public/javascripts/delete.js
+++ public/javascripts/delete.js
@@ -1,3 +1,16 @@
+document.getElementById("delete").onclick = function () {
+  const productId = document.getElementById("product-id").value;
+  axios
+    .delete(`/api/products/${productId}`)
+    .then(processResult)
+    .catch((err) => {
+      if (err.response.status === 404) {
+        notFound();
+      }
+    });
+
+};
+
 function processResult() {
   window.alert("Product deleted!");
 }
