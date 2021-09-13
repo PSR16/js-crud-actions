76ad408597a68b1d59bea90bad8637ff6f1a2dfc
Pegah <Prashidnia@gmail.com>
2021-09-12 21:15:29 -0400

    getting products from DB

--- routes/products.js
+++ routes/products.js
@@ -2,5 +2,8 @@ var express = require("express");
 var router = express.Router();
 
 module.exports = function (db) {
+  router.get("/products", (req, res) => {
+    res.send(db.get("products").value()); //use response object 
+  });
   return router;
 };
